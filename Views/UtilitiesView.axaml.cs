c570a4bf2d2252ea7f067f6f1e5eb3747cdf5815
tsgsOFFICIAL <marcusproman0507@gmail.com>
2026-07-15 11:27:39 +0200

    Added Utility page + Good to know resources.

---
+++ Views/UtilitiesView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+
+namespace RustOptimizer.Views;
+
+/// <summary>The Utilities page: a static grid of external Rust resource links.</summary>
+public partial class UtilitiesView : UserControl
+{
+    /// <summary>Creates the view.</summary>
+    public UtilitiesView()
+    {
+        InitializeComponent();
+    }
+}
