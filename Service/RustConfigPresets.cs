93fe0655c8553bdb707280e20fe7285e51d20ca7
tsgsOFFICIAL <marcusproman0507@gmail.com>
2026-07-21 18:51:11 +0200

    Updated presets - tested on high end machine

--- Service/RustConfigPresets.cs
+++ Service/RustConfigPresets.cs
@@ -32,47 +32,53 @@ internal static class RustConfigPresets
     private static readonly Dictionary<string, string> Common = new()
     {
         ["system.auto_cpu_affinity"] = "True",
-        ["graphics.contactshadows"] = "False",
         ["graphics.dof"] = "False",
         ["culling.toggle"] = "True",
         ["gc.buffer"] = "4096",
         ["fps.limit"] = "0",
-        ["graphics.screenmode"] = "0", // TODO: Verify 0 or 1 (I have 1 on my gaming rig but 0 is default)
+        ["graphics.screenmode"] = "1",
         ["rgbeffects.enabled"] = "False",
-        ["player.recoilcomp"] = "False"
+        ["player.recoilcomp"] = "False",
+        ["decor.quality"] = "0",
+        ["graphicssettings.softparticles"] = "True"
     };
 
     private static readonly Dictionary<string, string> LowEndPc = new()
     {
-        ["decor.quality"] = "0",
+        ["graphicssettings.globaltexturemipmaplimit"] = "2", // 1/4 Resolution
+        ["graphics.contactshadows"] = "False",
+        ["effects.antialiasing"] = "0",
         ["effects.ao"] = "False",
         ["effects.bloom"] = "False",
         ["effects.shafts"] = "False",
-        ["graphics.af"] = "1",
+        ["graphics.af"] = "4",
         ["graphics.drawdistance"] = "500",
         ["graphics.lodbias"] = "0.5",
         ["graphics.shaderlod"] = "1",
-        ["graphics.shadowlights"] = "0",
-        ["graphics.shadowmode"] = "1",
+        ["graphics.shadowlights"] = "1",
+        ["graphics.shadowmode"] = "3",
         ["graphics.volumetric_clouds"] = "0",
-        ["graphicssettings.particleraycastbudget"] = "0",
+        ["graphicssettings.particleraycastbudget"] = "4",
         ["graphicssettings.pixellightcount"] = "0",
-        ["graphicssettings.shadowqualitypreset"] = "0",
-        ["graphicssettings.softparticles"] = "False",
-        ["grass.quality"] = "0",
+        ["graphicssettings.shadowqualitypreset"] = "1",
         ["mesh.quality"] = "40",
+        ["grass.quality"] = "0",
         ["particle.quality"] = "0",
         ["render.instancing_render_distance"] = "500",
-        ["terrain.quality"] = "20",
-        ["tree.meshes"] = "20",
-        ["tree.quality"] = "20",
+        ["tree.meshes"] = "40",
+        ["tree.quality"] = "75",
+        ["terrain.quality"] = "50",
+        ["ui.showinventoryplayer"] = "False",
+        ["ui.inventoryplayerquality"] = "0",
         ["water.quality"] = "0",
         ["water.reflections"] = "0"
     };
 
     private static readonly Dictionary<string, string> Competitive = new()
     {
-        ["decor.quality"] = "0",
+        ["graphicssettings.globaltexturemipmaplimit"] = "1", // 1/2 Resolution
+        ["graphics.contactshadows"] = "False",
+        ["effects.antialiasing"] = "3",
         ["effects.ao"] = "True",
         ["effects.bloom"] = "False",
         ["effects.shafts"] = "False",
@@ -86,42 +92,46 @@ internal static class RustConfigPresets
         ["graphicssettings.particleraycastbudget"] = "4",
         ["graphicssettings.pixellightcount"] = "0",
         ["graphicssettings.shadowqualitypreset"] = "1",
-        ["graphicssettings.softparticles"] = "True",
-        ["grass.quality"] = "100",
         ["mesh.quality"] = "110",
+        ["grass.quality"] = "100",
         ["particle.quality"] = "100",
         ["render.instancing_render_distance"] = "1500",
-        ["terrain.quality"] = "75",
         ["tree.meshes"] = "100",
         ["tree.quality"] = "125",
+        ["terrain.quality"] = "75",
+        ["ui.showinventoryplayer"] = "True",
+        ["ui.inventoryplayerquality"] = "0",
         ["water.quality"] = "0",
         ["water.reflections"] = "0"
     };
 
     private static readonly Dictionary<string, string> Cinematic = new()
     {
-        ["decor.quality"] = "100",
+        ["graphicssettings.globaltexturemipmaplimit"] = "0", // 1/1 Resolution
+        ["graphics.contactshadows"] = "True",
+        ["effects.antialiasing"] = "3",
         ["effects.ao"] = "True",
         ["effects.bloom"] = "True",
         ["effects.shafts"] = "True",
         ["graphics.af"] = "16",
-        ["graphics.drawdistance"] = "2000",
-        ["graphics.lodbias"] = "3",
-        ["graphics.shaderlod"] = "6",
+        ["graphics.drawdistance"] = "1500",
+        ["graphics.lodbias"] = "2.5",
+        ["graphics.shaderlod"] = "5",
         ["graphics.shadowlights"] = "2",
-        ["graphics.shadowmode"] = "3",
-        ["graphics.volumetric_clouds"] = "3",
-        ["graphicssettings.particleraycastbudget"] = "256",
-        ["graphicssettings.pixellightcount"] = "3",
+        ["graphics.shadowmode"] = "4",
+        ["graphics.volumetric_clouds"] = "1",
+        ["graphicssettings.particleraycastbudget"] = "512",
+        ["graphicssettings.pixellightcount"] = "4",
         ["graphicssettings.shadowqualitypreset"] = "3",
-        ["graphicssettings.softparticles"] = "True",
+        ["mesh.quality"] = "125",
         ["grass.quality"] = "100",
-        ["mesh.quality"] = "200",
         ["particle.quality"] = "100",
         ["render.instancing_render_distance"] = "2000",
-        ["terrain.quality"] = "100",
-        ["tree.meshes"] = "150",
+        ["tree.meshes"] = "100",
         ["tree.quality"] = "150",
+        ["terrain.quality"] = "75",
+        ["ui.showinventoryplayer"] = "True",
+        ["ui.inventoryplayerquality"] = "2",
         ["water.quality"] = "2",
         ["water.reflections"] = "1"
     };
