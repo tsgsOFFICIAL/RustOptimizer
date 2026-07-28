3fcf88d84609e4676a05d35eebbf30b662adc0c5
tsgsOFFICIAL <marcusproman0507@gmail.com>
2026-07-28 23:45:49 +0200

    Fixed Low End PC and Competitive incorrectly showing Texture Quality as Custom the moment they were applied

--- Service/RecommendedGraphicsSliders.cs
+++ Service/RecommendedGraphicsSliders.cs
@@ -21,95 +21,158 @@ internal static class RecommendedGraphicsSliders
         new("GraphicsSliderShadowQualityLabel", "ShadowQuality",
         [
             new("GraphicsTierLow", "Low", [
-                new("graphics.shadowmode", "2"), new("graphics.shadowlights", "0"),
-                new("graphics.contactshadows", "False"), new("graphicssettings.shadowqualitypreset", "0")
+                new("graphics.shadowmode", "2"),
+                new("graphics.shadowlights", "1"),
+                new("graphics.contactshadows", "False"),
+                new("graphicssettings.shadowqualitypreset", "0"),
+                new("graphicssettings.particleraycastbudget", "4")
             ]),
             new("GraphicsTierMedium", "Medium", [
-                new("graphics.shadowmode", "3"), new("graphics.shadowlights", "1"),
-                new("graphics.contactshadows", "False"), new("graphicssettings.shadowqualitypreset", "1")
+                new("graphics.shadowmode", "3"),
+                new("graphics.shadowlights", "1"),
+                new("graphics.contactshadows", "False"),
+                new("graphicssettings.shadowqualitypreset", "1"),
+                new("graphicssettings.particleraycastbudget", "4")
             ]),
             new("GraphicsTierHigh", "High", [
-                new("graphics.shadowmode", "4"), new("graphics.shadowlights", "2"),
-                new("graphics.contactshadows", "True"), new("graphicssettings.shadowqualitypreset", "3")
+                new("graphics.shadowmode", "4"),
+                new("graphics.shadowlights", "2"),
+                new("graphics.contactshadows", "True"),
+                new("graphicssettings.shadowqualitypreset", "3"),
+                new("graphicssettings.particleraycastbudget", "512")
             ])
         ]),
 
         new("GraphicsSliderTextureQualityLabel", "TextureQuality",
         [
             new("GraphicsTierLow", "Low", [
-                new("graphicssettings.globaltexturemipmaplimit", "2"), new("graphics.af", "4"),
-                new("graphics.lodbias", "0.5"), new("graphics.shaderlod", "1")
+                new("graphicssettings.globaltexturemipmaplimit", "2"),
+                new("graphics.af", "1"),
+                new("graphics.lodbias", "0.5"),
+                new("graphics.shaderlod", "1"),
+                new("graphicssettings.anisotropicfiltering", "0")
             ]),
             new("GraphicsTierMedium", "Medium", [
-                new("graphicssettings.globaltexturemipmaplimit", "1"), new("graphics.af", "8"),
-                new("graphics.lodbias", "1"), new("graphics.shaderlod", "3")
+                new("graphicssettings.globaltexturemipmaplimit", "1"),
+                new("graphics.af", "1"),
+                new("graphics.lodbias", "1"),
+                new("graphics.shaderlod", "3"),
+                new("graphicssettings.anisotropicfiltering", "0")
             ]),
             new("GraphicsTierHigh", "High", [
-                new("graphicssettings.globaltexturemipmaplimit", "0"), new("graphics.af", "16"),
-                new("graphics.lodbias", "2.5"), new("graphics.shaderlod", "5")
+                new("graphicssettings.globaltexturemipmaplimit", "0"),
+                new("graphics.af", "8"),
+                new("graphics.lodbias", "2.5"),
+                new("graphics.shaderlod", "5"),
+                new("graphicssettings.anisotropicfiltering", "1")
             ])
         ]),
 
         new("GraphicsSliderEffectsQualityLabel", "EffectsQuality",
         [
             new("GraphicsTierLow", "Low", [
-                new("effects.antialiasing", "0"), new("effects.ao", "False"), new("effects.bloom", "False"),
-                new("effects.shafts", "False"), new("graphics.volumetric_clouds", "0"), new("graphicssettings.pixellightcount", "0")
+                new("effects.antialiasing", "0"),
+                new("effects.ao", "False"),
+                new("effects.bloom", "False"),
+                new("effects.shafts", "False"),
+                new("graphics.volumetric_clouds", "0"),
+                new("graphicssettings.pixellightcount", "0"),
+                new("particle.quality", "0")
             ]),
             new("GraphicsTierMedium", "Medium", [
-                new("effects.antialiasing", "3"), new("effects.ao", "True"), new("effects.bloom", "False"),
-                new("effects.shafts", "False"), new("graphics.volumetric_clouds", "0"), new("graphicssettings.pixellightcount", "0")
+                new("effects.antialiasing", "3"),
+                new("effects.ao", "True"),
+                new("effects.bloom", "False"),
+                new("effects.shafts", "False"),
+                new("graphics.volumetric_clouds", "0"),
+                new("graphicssettings.pixellightcount", "0"),
+                new("particle.quality", "100")
             ]),
             new("GraphicsTierHigh", "High", [
-                new("effects.antialiasing", "3"), new("effects.ao", "True"), new("effects.bloom", "True"),
-                new("effects.shafts", "True"), new("graphics.volumetric_clouds", "1"), new("graphicssettings.pixellightcount", "4")
+                new("effects.antialiasing", "3"),
+                new("effects.ao", "True"),
+                new("effects.bloom", "True"),
+                new("effects.shafts", "True"),
+                new("graphics.volumetric_clouds", "1"),
+                new("graphicssettings.pixellightcount", "4"),
+                new("particle.quality", "100")
             ])
         ]),
 
         new("GraphicsSliderDrawDistanceLabel", "DrawDistance",
         [
             new("GraphicsTierLow", "Low", [
-                new("graphics.drawdistance", "500"), new("render.instancing_render_distance", "500"),
-                new("graphicssettings.particleraycastbudget", "4"), new("particle.quality", "0")
+                new("graphics.drawdistance", "500"),
+                new("render.instancing_render_distance", "500")
             ]),
             new("GraphicsTierMedium", "Medium", [
-                new("graphics.drawdistance", "1000"), new("render.instancing_render_distance", "1500"),
-                new("graphicssettings.particleraycastbudget", "4"), new("particle.quality", "100")
+                new("graphics.drawdistance", "1000"),
+                new("render.instancing_render_distance", "1500")
             ]),
             new("GraphicsTierHigh", "High", [
-                new("graphics.drawdistance", "1500"), new("render.instancing_render_distance", "2000"),
-                new("graphicssettings.particleraycastbudget", "512"), new("particle.quality", "100")
+                new("graphics.drawdistance", "2000"),
+                new("render.instancing_render_distance", "2500")
             ])
         ]),
 
         new("GraphicsSliderWorldDetailLabel", "WorldDetail",
         [
             new("GraphicsTierLow", "Low", [
-                new("mesh.quality", "40"), new("grass.quality", "0"), new("tree.meshes", "40"),
-                new("tree.quality", "75"), new("terrain.quality", "50")
+                new("mesh.quality", "40"),
+                new("grass.quality", "0"),
+                new("tree.meshes", "40"),
+                new("tree.quality", "75"),
+                new("terrain.quality", "50")
             ]),
             new("GraphicsTierMedium", "Medium", [
-                new("mesh.quality", "110"), new("grass.quality", "100"), new("tree.meshes", "100"),
-                new("tree.quality", "125"), new("terrain.quality", "75")
+                new("mesh.quality", "110"),
+                new("grass.quality", "100"),
+                new("tree.meshes", "100"),
+                new("tree.quality", "125"),
+                new("terrain.quality", "75")
             ]),
             new("GraphicsTierHigh", "High", [
-                new("mesh.quality", "125"), new("grass.quality", "100"), new("tree.meshes", "100"),
-                new("tree.quality", "150"), new("terrain.quality", "75")
+                new("mesh.quality", "125"),
+                new("grass.quality", "100"),
+                new("tree.meshes", "100"),
+                new("tree.quality", "150"),
+                new("terrain.quality", "75")
             ])
         ]),
 
         new("GraphicsSliderWaterQualityLabel", "WaterQuality",
         [
-            new("GraphicsTierLow", "Low", [new("water.quality", "0"), new("water.reflections", "0")]),
-            new("GraphicsTierMedium", "Medium", [new("water.quality", "1"), new("water.reflections", "0")]),
-            new("GraphicsTierHigh", "High", [new("water.quality", "2"), new("water.reflections", "1")])
+            new("GraphicsTierLow", "Low", [
+                new("water.quality", "0"),
+                new("water.reflections", "0")
+            ]),
+            new("GraphicsTierMedium", "Medium", [
+                new("water.quality", "0"),
+                new("water.reflections", "1")
+            ]),
+            new("GraphicsTierHigh", "High", [
+                new("water.quality", "2"),
+                new("water.reflections", "2")
+            ])
         ]),
 
         new("GraphicsSliderInventoryDisplayLabel", "InventoryDisplay",
         [
-            new("GraphicsTierLow", "Low", [new("ui.showinventoryplayer", "False"), new("ui.inventoryplayerquality", "0")]),
-            new("GraphicsTierMedium", "Medium", [new("ui.showinventoryplayer", "True"), new("ui.inventoryplayerquality", "0")]),
-            new("GraphicsTierHigh", "High", [new("ui.showinventoryplayer", "True"), new("ui.inventoryplayerquality", "2")])
+            new("GraphicsTierLow", "Low",
+            [
+                new("ui.showinventoryplayer", "False"),
+                new("ui.inventoryplayerquality", "0")
+            ]),
+            new("GraphicsTierMedium", "Medium",
+            [
+                new("ui.showinventoryplayer", "True"),
+                new("ui.inventoryplayerquality", "0")
+            ]),
+            new("GraphicsTierHigh", "High",
+            [
+                new("ui.showinventoryplayer", "True"),
+                new("ui.inventoryplayerquality", "2")
+            ])
         ])
     ];
 }
